03245b6827608d14249f40bb3ae203c00d42b4af
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-13 02:09:14 +0300

    Update yafa.py

--- YukkiMusic/plugins/tools/yafa.py
+++ YukkiMusic/plugins/tools/yafa.py
@@ -50,6 +50,18 @@ def ReplyCheck(message: Message):
     return reply_id
 
 
+session = ClientSession()
+pattern = re.compile(r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$")
+BASE = "https://batbin.me/"
+
+async def post(url: str, *args, **kwargs):
+    async with session.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
 async def paste(content: str):
     resp = await post(f"{BASE}api/v2/paste", data=content)
     if not resp["success"]:
