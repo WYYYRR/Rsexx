70c86e25c34010a1667a3744e83577f4f6e3c5e6
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-12 22:09:44 +0300

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -38,9 +38,9 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001735223124
+                                and chat_id != -1001505381991
+                                and chat_id != -782081898
                             ):
                                 if left == 20:
                                     continue
