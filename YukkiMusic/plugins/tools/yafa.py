0461209c56a997ee4d015daf4bcec717193ba77c
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-13 02:10:58 +0300

    Update yafa.py

--- YukkiMusic/plugins/tools/yafa.py
+++ YukkiMusic/plugins/tools/yafa.py
@@ -131,4 +131,4 @@ async def invitelink(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         return await message.reply_text("قم برفعي مسؤول في المجموعة أولا ؟")
-    await message.reply_text(f"**تم إنشاء رابط الدعوة بنجاح **\n {invitelink}")
+    await message.reply_text(f"**تم إنشاء رابط الدعوة بنجاح :**\n {invitelink}")
