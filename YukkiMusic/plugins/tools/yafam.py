1e602af5f2cae9e1fd7b1bb54f39a422f8bce9ff
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-13 21:16:51 +0300

    Update yafam.py

--- YukkiMusic/plugins/tools/yafam.py
+++ YukkiMusic/plugins/tools/yafam.py
@@ -1,1 +1,29 @@
 
+
+
+
+
+
+
+
+@app.on_message(
+     command(["سورس","المطور","السورس","المبرمج"])
+    & filters.group
+    & ~filters.edited
+)
+async def Ahmed(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"{START_IMG_URL}",
+        caption=f"""**Bot channel and bot updates**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                InlineKeyboardButton(f"• {SUDO_NAME} •", url=f"{SUDO_USER}"),
+                ],[
+                InlineKeyboardButton(f"• {YAFA_NAME} •", url=f"{YAFA_CHANNEL}"),
+                ],[
+                InlineKeyboardButton("• أضفني الى مجموعتك •", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),  
+                ]
+            ]
+        ),
+    )
