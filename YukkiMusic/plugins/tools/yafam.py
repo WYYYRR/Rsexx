e76b30ea2e10c348ed041bd8f1f6b79fc43dfca6
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-13 21:51:32 +0300

    Update yafam.py

--- YukkiMusic/plugins/tools/yafam.py
+++ YukkiMusic/plugins/tools/yafam.py
@@ -1,9 +1,31 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from pyrogram import filters, Client
+from YukkiMusic.utils.database import is_music_playing, music_off
+from strings import get_command
+import asyncio
 from strings.filters import command
-from config import (START_IMG_URL, SUDO_NAME, SUDO_USER,
-                    YAFA_NAME, YAFA_CHANNEL, BOT_USERNAME)
 from YukkiMusic import app
+from YukkiMusic.core.call import Yukki
+from YukkiMusic.utils.database import set_loop
+from YukkiMusic.utils.decorators import AdminRightsCheck
+from YukkiMusic.utils.database import is_muted, mute_on
+from YukkiMusic.utils.database import is_muted, mute_off
+from YukkiMusic.utils.database import is_music_playing, music_on
+from datetime import datetime
+from config import (BANNED_USERS, MUSIC_BOT_NAME, YAFA_NAME, YAFA_CHANNEL,
+                    SUDR_USER, SUDO_NAMElyrical, START_IMG_URL, MONGO_DB_URI, OWNER_ID)
+from YukkiMusic.utils import bot_sys_stats
+from YukkiMusic.utils.decorators.language import language
+import random
+import config
+import re
+import string
+import lyricsgenius as lg
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
+from pyrogram import Client, filters
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from typing import Union
+import sys
+from os import getenv
 
 @app.on_message(
      command(["سورس","المطور","السورس","المبرمج"])
