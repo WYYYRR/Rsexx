7d0b130fc39582ec84ec8cfe185097b5aa336e41
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-16 00:00:40 +0300

    Update must.join.py

--- YukkiMusic/plugins/tools/must.join.py
+++ YukkiMusic/plugins/tools/must.join.py
@@ -1,32 +1,33 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from Config import MUST_JOIN
+from Config import YAFA_CHANNEL, YAFA_NAME, CHANNEL_SUDO
+from YukkiMusic import app
 
 
 @Client.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not MUST_JOIN:  # Not compulsory
+    if not YAFA_CHANNEL:  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+            await bot.get_chat_member(CHANNEL_SUDO, msg.from_user.id)
         except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+            if YAFA_CHANNEL.isalpha():
+                link = "https://t.me/" + YAFA_CHANNEL
             else:
-                chat_info = await bot.get_chat(MUST_JOIN)
+                chat_info = await bot.get_chat(CHANNEL_SUDO)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"- عذراً !! عليك الأشتراك بقناة البوت أولاً :",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                        [InlineKeyboardButton(u"{YAFA_NAME}", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : {YAFA_CHANNEL} !")
